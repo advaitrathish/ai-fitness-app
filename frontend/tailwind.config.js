08b96d619319345e209a82de625f450199bd49d6
Advait Rathish <advaitrathish@gmail.com>
2026-02-21 16:07:49 +0530

    Added route groups and layout structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // THIS IS THE FIX
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
